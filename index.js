f56d0a14908b0ec2ebb7948091ac4b567bf1d090
serendipityerr <95729345+serendipityerr@users.noreply.github.com>
2025-08-01 09:03:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ const readme = `\
 ---
 ⏰ Updated on ${new Date().toUTCString()}
 ---
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/serendipityerr/serendipityerr/output/github-contribution-grid-snake-dark.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/serendipityerr/serendipityerr/output/github-contribution-grid-snake.svg">
+  <img alt="github contribution grid snake animation" src="https://raw.githubusercontent.com/serendipityerr/serendipityerr/output/github-contribution-grid-snake.svg">
+</picture>
+
+<div align="left">
+<picture>
+  <source
+    srcset="https://github-readme-stats.vercel.app/api?username=serendipityerr&show_icons=true&hide_border=true&line_height=24&theme=buefy"
+    media="(prefers-color-scheme: radical)"
+  />
+  <img src="https://github-readme-stats.vercel.app/api?username=serendipityerr&show_icons=true&hide_border=true&line_height=24&theme=buefy" />
+</picture>
+<picture>
+  <img src="./cat.gif" width="335">
+</picture>
+</div>
